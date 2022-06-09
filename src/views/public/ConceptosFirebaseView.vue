f8a3ba7d8a9d75a94efa63a04b2d52b619aa8eb9
webferrol <xurxo@webferrol.com>
2022-06-09 14:04:23 +0200

    Restructurando el contenido de Home

---
+++ src/views/public/ConceptosFirebaseView.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="home content is-medium p-5">
+        <h1 class="title has-text-centered">Conceptos Firebase</h1>
+        <div class="columns is-multiline">
+            <!-- Contenido 1 -->
+            <div class="column is-one-third">
+                <div class="tile is-ancestor">
+                    <div class="tile is-parent">
+                        <article class="tile is-child notification is-warning">
+                            <div class="content">
+                                <p class="title">Firebase: usuarios</p>
+                                <a class="subtitle" href="https://firebase.google.com/docs/auth/web/manage-users"
+                                    target="_blank">
+                                    Administra usuarios en Firebase: creación, gestión, eliminación...</a>
+                                <div class="content">
+                                    <!-- Content -->
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
+            <!-- Fin contenido 1 -->
+            <!-- Contenido 2 -->
+            <div class="column is-one-third">
+                <div class="tile is-ancestor">
+                    <div class="tile is-parent">
+                        <article class="tile is-child notification is-success">
+                            <div class="content">
+                                <p class="title">Firebase: Storage</p>
+                                <a class="subtitle"
+                                    href="https://firebase.google.com/docs/storage/web/start?hl=es&authuser=0"
+                                    target="_blank">
+                                    Gestiona ficheros, imágenes en el Cloud Storage ...</a>
+                                <div class="content">
+                                    <!-- Content -->
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
+            <!-- Fin contenido 2 -->
+            <!-- Contenido 3 -->
+            <div class="column is-one-third">
+                <div class="tile is-ancestor">
+                    <div class="tile is-parent">
+                        <article class="tile is-child notification is-info">
+                            <div class="content">
+                                <p class="title">Cloud Firestore</p>
+                                <a class="subtitle" href="https://firebase.google.com/docs/firestore" target="_blank">
+                                    Base de datos NoSQL flexible, escalable y en la nube </a>
+                                <div class="content">
+                                    <!-- Content -->
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
+            <!-- Fin contenido 3 -->
+        </div>
+    </div>
+</template>
